0c52fa83995fcc8342a5caab1289a36c9887da3c
Hariprasath <126390300+Hari7070@users.noreply.github.com>
2024-08-01 21:09:33 +0530

    Update settings.py

--- idea_project/settings.py
+++ idea_project/settings.py
@@ -6,8 +6,8 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-g2vg_mc1#+t(++=@4_jy8uqbn8=1hxm@u)u*8*6f=ox&f*6o$y'
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
